a61656c86dde0202c8f303163e3bbc26886c9311
leekyuyoung20230520 <134119758+leekyuyoung20230520@users.noreply.github.com>
2023-05-20 13:42:30 +0900

    Add files via upload

---
+++ Test3.java
@@ -0,0 +1,12 @@
+package test;
+
+public class Test3 {
+
+	public static void main(String[] args) {
+		int num1 = 5;
+		int num2 = 2;
+		System.out.println(num1 / num2);
+		System.out.println( (double)num1 / (double)num2  );
+	}
+
+}
