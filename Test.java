1a9e0d20f72a935f34c1c0554645822893c045e9
leekyuyoung20230520 <134119758+leekyuyoung20230520@users.noreply.github.com>
2023-05-20 12:31:22 +0900

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,15 @@
+package test;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//아래 문장은 한글이라는 단어를 콘솔에 출력하기 위한 명령어입니다.
+		System.out.println("한글");
+//		System.out.println("영어");
+		System.out.println("abc");		 
+		
+		
+	}
+
+}
---
+++ Test2.java
@@ -0,0 +1,12 @@
+package test;
+
+public class Test2 {
+
+	public static void main(String[] args) {
+		String name = "kyu young lee";
+		System.out.println(name);
+		
+
+	}
+
+}
