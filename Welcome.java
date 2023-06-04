af62e2a9b93ae784394f654b2360cd55d3c264da
leekyuyoung20230520 <134119758+leekyuyoung20230520@users.noreply.github.com>
2023-06-04 11:34:20 +0900

    Add files via upload

--- Welcome.java
+++ Welcome.java
@@ -1,28 +1,75 @@
+import java.util.Scanner;
 
 public class Welcome {
-
-	public static void main(String[] args) {
-		String greeting = "Welcome to Shoopping Mall";
-		String greeting2 = "Welcome to Book Market";
-		String menu1 = "1. 고객정보 확인하기";
-		String menu2 = "2. 장바구니 상품 목록 보기";
-		String menu3 = "3. 장바구니 비우기";
-		String menu4 = "4. 장바구니에 항목 추가하기";
-		String menu5 = "5. 장바구니에 항목 수량 줄이기";
-		String menu6 = "6. 장바구니에 항목 삭제하기";
-		String menu7 = "7. 영수증 표기하기";
-		String menu8 = "8. 종료";
-		
-		System.out.println("************************************************************");
-		System.out.println("\t\t"+greeting);
-		System.out.println("\t\t"+greeting2);
-		System.out.println("************************************************************");
-		System.out.println(menu1 + "\t" + menu4+"\n");
-		System.out.println(menu2 + "\t" + menu5+"\n");
-		System.out.println(menu3 + "\t\t" + menu6+"\n");
-		System.out.println(menu7 + "\t\t" + menu8+"\n");
-		System.out.println("************************************************************");
-
+	// 프로그램 정보를 디스플레이 하는 함수
+	public static void displayIntro() {
+		System.out.println("***********************************************");
+		System.out.println("******* Welcome to Shopping Mall **************");
+		System.out.println("******* Welcome to Book Market  ***************");
 	}
+	// 메뉴정보를 디스플레이 하는 메소드 만들기 : 
+	public static void displayMenu() {
+		System.out.println("***********************************************");
+		System.out.println("1.고객정보 확인하기\t\t4. 장바구니에 항목 추가하기");
+		System.out.println("2.장바구니 상품 목록 보기\t5. 장바구니에 항목 수량 줄이기");
+		System.out.println("3.장바구니 비우기\t\t6. 장바구니 항목 삭제하기");
+		System.out.println("7.영수증 표시하기\t\t8. 종료");
+		System.out.println("***********************************************");
+	}
+	// 1.고객정보 확인하기
+	public static void menuGuestInfo(String name, String phone) {
+		System.out.println("현재 고객정보 : ");
+		System.out.printf("이름 %s		연락처 %s\n",name,phone);
+	}
+	// 2.장바구니 상품 목록 보기
+	public static void menuCartItemList() {} 
+	// 3.장바구니 비우기
+	public static void menuCartClear() {}
+	// 4. 장바구니에 항목 추가하기
+	public static void menuCartAddItem() {}
+	// 5. 장바구니에 항목 수량 줄이기
+	public static void menuCartRemoveItemCount() {}
+	// 6. 장바구니 항목 삭제하기
+	public static void menuCartRemoveItem() {}
+	// 7.영수증 표시하기
+	public static void menuCartBill() {}
+	// 8. 종료
+	public static void menuExit() {}
+	
+	public static void main(String[] args) {
+		System.out.print("이름을 입력하세요 :");
+		Scanner sc = new Scanner(System.in);
+		String name = sc.next();		
+		System.out.print("연락처를 입력하세요 :");
+		String phone = sc.next();
+		displayIntro(); // 프로그램 정보 출력
+		while(true) {			
+			displayMenu(); // 메뉴출력
+			System.out.print("메뉴 번호를 선택 해 주세요 : ");
+			int choiceMenu = sc.nextInt();
+			switch (choiceMenu) {
+			case 1:			
+				menuGuestInfo(name,phone);
+				break;
+			case 2:			
+				break;
+			case 3:			
+				break;
+			case 4:			
+				break;
+			case 5:			
+				break;
+			case 6:			
+				break;
+			case 7:			
+				break;
+			case 8:
+				System.out.println("이용해 주셔서 감사합니다.");
+				return;				
+			default:
+				break;
+			} // end switch	
+		} // end while		
+	} // end main
 
-}
+} // end class
