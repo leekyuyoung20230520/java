73c2b7caf50230aa4f3faac17a76f01e1ac13573
leekyuyoung20230520 <134119758+leekyuyoung20230520@users.noreply.github.com>
2023-06-04 12:33:32 +0900

    Add files via upload

--- Welcome.java
+++ Welcome.java
@@ -34,37 +34,51 @@ public class Welcome {
 	// 7.영수증 표시하기
 	public static void menuCartBill() {}
 	// 8. 종료
-	public static void menuExit() {}
-	
-	public static void main(String[] args) {
-		System.out.print("이름을 입력하세요 :");
+	public static void menuExit() {
+		System.out.println("이용해 주셔서 감사합니다.");
+		return;
+	}
+	// 사용자 입력을 처리하는 함수
+	public static String getData(String str) {
+		System.out.println(str);
 		Scanner sc = new Scanner(System.in);
-		String name = sc.next();		
-		System.out.print("연락처를 입력하세요 :");
-		String phone = sc.next();
+		String data = sc.next();
+		return data;
+	}
+	
+	public static void main(String[] args) {		
+		String name = getData("이름을 입력하세요");
+		String phone = getData("전화번호를 입력하세요");
+		
+		
 		displayIntro(); // 프로그램 정보 출력
 		while(true) {			
-			displayMenu(); // 메뉴출력
-			System.out.print("메뉴 번호를 선택 해 주세요 : ");
-			int choiceMenu = sc.nextInt();
+			displayMenu(); // 메뉴출력			
+			int choiceMenu = Integer.valueOf( getData("메뉴 번호를 선택 해 주세요") );
 			switch (choiceMenu) {
 			case 1:			
 				menuGuestInfo(name,phone);
 				break;
 			case 2:			
+				menuCartItemList();
 				break;
-			case 3:			
+			case 3:		
+				menuCartClear();
 				break;
 			case 4:			
+				menuCartAddItem();
 				break;
 			case 5:			
+				menuCartRemoveItemCount();
 				break;
 			case 6:			
+				menuCartRemoveItem();
 				break;
-			case 7:			
+			case 7:		
+				menuCartBill();
 				break;
 			case 8:
-				System.out.println("이용해 주셔서 감사합니다.");
+				menuExit();				
 				return;				
 			default:
 				break;
