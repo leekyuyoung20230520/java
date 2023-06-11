cf0ce4703ada3dfe69d97c90dbfdee4ba4ad9c44
leekyuyoung20230520 <134119758+leekyuyoung20230520@users.noreply.github.com>
2023-06-11 11:18:27 +0900

    Add files via upload

--- Welcome.java
+++ Welcome.java
@@ -3,8 +3,10 @@ import java.util.Scanner;
 public class Welcome {
 	public static final int NUM_BOOK = 3;
 	public static final int NUM_ITEM = 7;
+	public static String[][] booklist = new String[3][7];
 	// 기본 책 정보를 생성하는 메소드
 	public static void bookList(String[][] book) {
+		
 		book[0][0] = "ISBN1234";book[0][1] = "제목1";book[0][2] = "27000";
 		book[0][3] = "저자1";book[0][4] = "요약내용1";book[0][5] = "IT서적";
 		book[0][6] = "2023/05/02";
@@ -49,8 +51,57 @@ public class Welcome {
 	public static void menuCartClear() {
 	}
 
+	// 도서목록 출력
+	public static void displayBooks() {
+		for (int i = 0; i < booklist.length; i++) {
+			for (int j = 0; j < booklist[0].length; j++) { // 구성요소인 1차원 배열의 개수
+				System.out.print(booklist[i][j] + " | ");
+			}
+			System.out.println();
+		}	
+	}
+	//  장바구니에 item 추가하기
+	public static void cartAddItem() {
+		while(true) {
+			System.out.println("장바구니에 추가할 도서의 ID를 입력하세요");
+			Scanner sc = new Scanner(System.in);
+			String id = sc.nextLine();			
+			// 찾기
+			int numId = -1;  // 2차원배열에서 주어진 id로 찾은 배열의 인덱스(행,row)		
+			for (int i = 0; i < booklist.length; i++) {
+				if (booklist[i][0].equals(id) ) {
+					numId = i;				
+				}
+			}
+			// 장바구니에 도서 추가
+			if( numId != -1  ) 
+			{
+				System.out.println("장바구니에 추가 하시겠습니까? Y | N");
+				String yesNo = sc.nextLine();
+				if(yesNo.toUpperCase().equals("Y")) {
+					System.out.println(booklist[numId][0] + " 도서가 장바구니에 추가되었습니다.");					
+				}
+				break;
+			}else {
+				System.out.println("잘못 입력했습니다.계속 하시겠습니까? Y | N");
+				String yesNo = sc.nextLine();
+				if(!yesNo.toUpperCase().equals("Y")) {
+					break;
+				}				
+			}
+		}
+	}
 	// 4. 장바구니에 항목 추가하기
 	public static void menuCartAddItem() {
+		// 도서 생성 및 저장
+		bookList(booklist);
+		// booklist 메소드에 의해서 생성된 booklist 2차원 배열을 출력
+//		booklist.length  --> 2차원 배열의 개수(행,row)
+		// 도서목록 출력
+		displayBooks();	
+		// 도서id 입력, 장바구니에 아이템 추가하기
+		cartAddItem();
+		
 	}
 
 	// 5. 장바구니에 항목 수량 줄이기
