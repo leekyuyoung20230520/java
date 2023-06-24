ab0d8abfe0eb80604d012f08c19e64ec41a4fb5a
leekyuyoung20230520 <134119758+leekyuyoung20230520@users.noreply.github.com>
2023-06-24 13:30:39 +0900

    Add files via upload

--- 다형성/Demo3.java
+++ 다형성/Demo3.java
@@ -1,10 +1,31 @@
 package 다형성;
 
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
 public class Demo3 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		Enterance enterance = new Enterance();
+		boolean isNotFull = true;
+		int count = 1;
+		while (isNotFull) {
+			System.out.print("input age : ");
+			int age = Integer.valueOf(sc.nextLine());
+			isNotFull = enterance.checkAge(age);
+			if(count++ % 5 == 0) {
+				System.out.println("is Continue?(y/Y) :");
+				String isContinue = sc.nextLine().toUpperCase();
+				if(!isContinue.equals("Y"))
+					break;
+			}			
+		}
+		// 정산
+		DecimalFormat formatter = new DecimalFormat("#,###");
+		String totalPrice = formatter.format(enterance.getTotalPrice());
+		System.out.printf("총 수입 : %s원  입장객 수 : %d\n", 
+				totalPrice,enterance.getTotalEnters());
 	}
 
 }
