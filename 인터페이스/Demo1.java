d891d9447132394452f4de08ff70449aca9040a4
leekyuyoung20230520 <134119758+leekyuyoung20230520@users.noreply.github.com>
2023-06-24 13:46:49 +0900

    Add files via upload

---
+++ 인터페이스/Demo1.java
@@ -0,0 +1,15 @@
+package 인터페이스;
+/*
+ * 완전 추상화 클래스
+ * 	맴버메소드들이 일반 메소드는 없고 전부 추상 메소드 들만 존재할때
+ */
+public class Demo1 {
+
+	public static void main(String[] args) {
+		int a = Drawable.number;
+		
+				
+
+	}
+
+}
---
+++ 인터페이스/Drawable.java
@@ -0,0 +1,6 @@
+package 인터페이스;
+
+public interface Drawable {
+	public static final int number = 0;
+	
+}
