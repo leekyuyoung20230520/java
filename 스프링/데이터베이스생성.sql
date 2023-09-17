640737cefd6121b715a364505b1ba277d73d5d28
leekyuyoung20230520 <134119758+leekyuyoung20230520@users.noreply.github.com>
2023-09-17 13:44:49 +0900

    Add files via upload

---
+++ 스프링/데이터베이스생성.sql
@@ -0,0 +1,22 @@
+drop database if exists bookrental ;
+create database bookrental;
+use bookrental;
+
+CREATE TABLE tbl_admin_member(
+	a_m_no		INT 	AUTO_INCREMENT, 
+	a_m_approval	INT	NOT NULL DEFAULT 0, 
+	a_m_id		VARCHAR(20) 	NOT NULL, 
+	a_m_pw		VARCHAR(100) 	NOT NULL, 
+	a_m_name	VARCHAR(20) 	NOT NULL, 
+	a_m_gender	CHAR(1) 	NOT NULL, 
+	a_m_part	VARCHAR(20) 	NOT NULL,
+	a_m_position	VARCHAR(20) 	NOT NULL,
+	a_m_mail	VARCHAR(50) 	NOT NULL, 
+	a_m_phone	VARCHAR(20) 	NOT NULL, 
+	a_m_reg_date 	DATETIME, 
+	a_m_mod_date	DATETIME, 
+	PRIMARY KEY(a_m_no)
+	);
+    
+    
+select * from tbl_admin_member;    
