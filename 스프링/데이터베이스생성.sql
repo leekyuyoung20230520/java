3cbcb370ae45adc237024ac67c623395be505bab
leekyuyoung20230520 <134119758+leekyuyoung20230520@users.noreply.github.com>
2023-10-08 11:22:55 +0900

    Update 데이터베이스생성.sql

--- 스프링/데이터베이스생성.sql
+++ 스프링/데이터베이스생성.sql
@@ -20,3 +20,19 @@ CREATE TABLE tbl_admin_member(
     
     
 select * from tbl_admin_member;    
+
+
+CREATE TABLE tbl_user_member(
+	u_m_no		INT 	AUTO_INCREMENT, 	
+	u_m_id		VARCHAR(20) 	NOT NULL, 
+	u_m_pw		VARCHAR(100) 	NOT NULL, 
+	u_m_name	VARCHAR(20) 	NOT NULL, 
+	u_m_gender	CHAR(1) 	NOT NULL, 		
+	u_m_mail	VARCHAR(50) 	NOT NULL, 
+	u_m_phone	VARCHAR(20) 	NOT NULL, 
+	u_m_reg_date 	DATETIME, 
+	u_m_mod_date	DATETIME, 
+	PRIMARY KEY(u_m_no)
+	);
+    
+    select * from tbl_user_member;
